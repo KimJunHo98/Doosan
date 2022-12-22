99820d7b3a564cdba01c32eeb9dc8db425ceff80
KimJunHo98 <dydgnscc6845@gmail.com>
2022-12-22 14:53:25 +0900

    css 모바일 메뉴 버튼과 모바일 드롭다운 메뉴 추가, 모바일버튼 jQuery적용

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,36 @@
 $(function(){
     // 풀다운메뉴
-    $(".full_menu, ul.nav li").mouseover(function(){
+    $(".full_menu, .nav li").on("mouseover", function(){
         $(".full_menu").stop().slideDown(500);
     });
-    $(".full_menu, ul.nav li").mouseleave(function(){
+    $(".full_menu, .nav li").on("mouseleave", function(){
         $(".full_menu").stop().slideUp(500);
     });
 
     // nav
-    $(".full_cover > ul > li").mouseover(function(){
+    $(".full_cover > ul > li").on("mouseover", function(){
         var i = $(this).index();
         
         $("ul.nav li").eq(i).find(".line").css("width","100%");
     });
-    $(".full_cover > ul > li").mouseleave(function(){
+    $(".full_cover > ul > li").on("mouseleave", function(){
         var i = $(this).index();
         
-        $("ul.nav li").eq(i).find(".line").css("width","0");
+        $(".nav li").eq(i).find(".line").css("width","0");
     });
 
-    // 
-    $("ul.nav li").mouseover(function(){
+    // 풀다운 메뉴에 오버 했을 때
+    $(".nav li").on("mouseover", function(){
         $(this).find(".line").css("width", "100%");
     });
-    $("ul.nav li").mouseleave(function(){
+    $(".nav li").on("mouseleave", function(){
         $(this).find(".line").css("width","0");
     });
+
+    // 모바일 메뉴버튼 클릭 했을 때
+    $("#header .mo").on("click", function(e){
+        e.preventDefault();
+        $("#header .mo_menu").slideToggle(300);
+    });
+    
 });
